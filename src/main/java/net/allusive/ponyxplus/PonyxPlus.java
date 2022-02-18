183f188430773f87f695b26c8dcf51f1f87315d2
Alllusive <allusivedxdco@gmail.com>
2022-02-18 11:24:31 +1100

    mooo

--- src/main/java/net/allusive/ponyxplus/PonyxPlus.java
+++ src/main/java/net/allusive/ponyxplus/PonyxPlus.java
@@ -1,19 +1,16 @@
 package net.allusive.ponyxplus;
 
-
-import net.allusive.ponyxplus.block.*;
 import net.allusive.ponyxplus.block.ModBlocks;
 import net.allusive.ponyxplus.block.entity.NetherConduitBlockEntity;
 import net.allusive.ponyxplus.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 import static net.allusive.ponyxplus.block.ModBlocks.NETHER_CONDUIT;
 import static net.allusive.ponyxplus.block.ModBlocks.BE_NETHER_CONDUIT;
